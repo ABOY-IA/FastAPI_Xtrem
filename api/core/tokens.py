66cf62f941aa0c1d9363a3e917efa0a5cd19b8be
ABOY-IA <arnaud.boy.ia@gmail.com>
2025-05-20 17:55:39 +0200

    correction generation access token pour dernier pytest, à essayer.

--- api/core/tokens.py
+++ api/core/tokens.py
@@ -1,13 +1,17 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import jwt
+import uuid
 
 SECRET_KEY = os.getenv("SECRET_KEY", "mon_secret_default")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
-def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
+def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    to_encode.update({"iat": datetime.now(timezone.utc)})  # Ajoute l'instant de création
+    to_encode.update({"jti": str(uuid.uuid4())})           # Ajoute un identifiant unique
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
